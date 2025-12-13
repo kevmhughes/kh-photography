83f7841cbe1f1a02cf70c0d3901afd5b1373b6d5
Kevin Hughes <kevhughes24@hotmail.com>
2025-12-13 13:45:05 +0100

    Fix issue with Vercel dev so that Vercel accesses .env.local for API handler

--- frontend/api/articles/[id].js
+++ frontend/api/articles/[id].js
@@ -1,9 +1,3 @@
-/* VERCEL DEPLOY  */
-/* VERCEL DEPLOY  */
-/* VERCEL DEPLOY  */
-/* 1. Don't expose the token when deploying */
-/* 2. add frontend folder to deploy route */
-
 import axios from "axios";
 
 export default async function handler(req, res) {
@@ -22,29 +16,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Failed to fetch products" });
   }
 }
-
-/* VERCEL DEV  */
-/* VERCEL DEV  */
-/* VERCEL DEV  */
-
-/* import axios from "axios";
-
-export default async function handler(req, res) {
-  const { id } = req.query;
-
-  try {
-    const response = await axios.get(
-      `https://api.spreadconnect.app/articles/${id}`,
-      {
-        headers: {
-          "X-SPOD-ACCESS-TOKEN": "",
-        }
-      }
-    );
-
-    res.status(200).json(response.data);
-  } catch (err) {
-    console.error("API error:", err.response?.data || err.message);
-    res.status(500).json({ error: "Failed to fetch product" });
-  }
-}  */
