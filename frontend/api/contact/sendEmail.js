9796ec108be3bea90469c37511a20072af3079f1
Kevin Hughes <kevhughes24@hotmail.com>
2026-02-23 00:43:31 +0100

    Comment out g-recaptcha-response in email params
    Stop including the reCAPTCHA token in the EmailJS template_params in frontend/api/contact/sendEmail.js. The "g-recaptcha-response": token entry was commented out to avoid sending the token in the outgoing request while keeping the line for reference in case it needs to be restored.

--- frontend/api/contact/sendEmail.js
+++ frontend/api/contact/sendEmail.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
           template_id: process.env.EMAILJS_TEMPLATE_ID,
           user_id: process.env.EMAILJS_PUBLIC_KEY,
           accessToken: process.env.EMAILJS_PRIVATE_KEY,
-          template_params: { user_name, user_email, message, "g-recaptcha-response": token },
+          template_params: { user_name, user_email, message, /* "g-recaptcha-response": token  */},
         }),
       }
     );
